1182325c869b3437e2340efe3fe6769fe4abc443
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-09-25 23:29:54 -0300

    chore: remove radix ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,7 @@
-import { InfoCircledIcon } from '@radix-ui/react-icons';
-import { Callout, Flex } from '@radix-ui/themes';
-
 export default function App() {
   return (
     <>
-      <Flex direction="column" gap="3" align="start">
-        <Callout.Root size="1">
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>You will need admin privileges to install and access this application.</Callout.Text>
-        </Callout.Root>
-
-        <Callout.Root size="2">
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>You will need admin privileges to install and access this application.</Callout.Text>
-        </Callout.Root>
-
-        <Callout.Root size="3">
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>You will need admin privileges to install and access this application.</Callout.Text>
-        </Callout.Root>
-      </Flex>
+      <h1>app tsx</h1>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { Theme } from '@radix-ui/themes';
-import '@radix-ui/themes/styles.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LandingPage from './pages/lading-page';
 import Logging from './pages/logging';
@@ -25,8 +23,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme>
-      <RouterProvider router={router} />
-    </Theme>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
