48a18bc1c47715eb3bd9fc46571aa4eb8dc495cf
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-10-22 00:57:32 -0300

    feat: add basic styles

--- src/components/RecipeForm/index.tsx
+++ src/components/RecipeForm/index.tsx
@@ -17,9 +17,9 @@ const createRecipeFormSchema = z.object({
         .join(' ');
     }),
   description: z.string().nonempty('a descrição é obrigatorio'),
-  preparation_time: z.string().min(6, 'mínimo 6 caracteres').nonempty('campo obrigatório'),
-  cooking_time: z.string().min(6, 'mínimo 6 caracteres').nonempty('campo obrigatório'),
-  servings: z.string().min(6, 'mínimo 6 caracteres').nonempty('campo obrigatório'),
+  preparation_time: z.string().min(2, 'mínimo 2 caracteres').nonempty('campo obrigatório'),
+  cooking_time: z.string().min(2, 'mínimo 2 caracteres').nonempty('campo obrigatório'),
+  servings: z.string().min(2, 'mínimo 2 caracteres').nonempty('campo obrigatório'),
   preparation: z
     .array(
       z.object({
@@ -69,63 +69,114 @@ export function RecipeForm() {
   return (
     <>
       <div className="border-2 p-4 col-span-12">
-        <form onSubmit={handleSubmit(createRecipe)}>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input {...register('name')} className="border border-zinc-600" type="text" />
-            {errors.name && <span>{errors.name.message}</span>}
-          </div>
-          <div>
-            <label htmlFor="description">Description</label>
-            <input {...register('description')} className="border border-zinc-600" type="text" />
-            {errors.description && <span>{errors.description.message}</span>}
-          </div>
-          <div>
-            <label htmlFor="preparation_time">Preparation time</label>
-            <input {...register('preparation_time')} className="border border-zinc-600" type="text" />
-            {errors.preparation_time && <span>{errors.preparation_time.message}</span>}
-          </div>
-          <div>
-            <label htmlFor="cooking_time">Cooking time</label>
-            <input {...register('cooking_time')} className="border border-zinc-600" type="text" />
-            {errors.cooking_time && <span>{errors.cooking_time.message}</span>}
-          </div>
-          <div>
-            <label htmlFor="servings">Servings</label>
-            <input {...register('servings')} className="border border-zinc-600" type="text" />
-            {errors.servings && <span>{errors.servings.message}</span>}
-          </div>
-          <div>
-            <label htmlFor="preparation">
-              Preparation
-              <button type="button" onClick={addNewTech} className="bg-emerald-500 rounded font-semibold p-1">
-                adicionar
-              </button>
-            </label>
-            {fields.map((field, index) => (
-              <div key={field.id} className="flex items-center gap-2">
-                <span>{`Order: ${index + 1}`}</span>
+        <div className="gap-4 grid grid-cols-12">
+          <div className="border-2 p-4 col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6">
+            <form onSubmit={handleSubmit(createRecipe)} className="flex flex-col gap-4">
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="name">
+                  Name
+                </label>
                 <input
-                  {...register(`preparation.${index}.preparation`)}
-                  className="border border-zinc-600"
+                  {...register('name')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   type="text"
                 />
-                {errors.preparation?.[index]?.preparation && (
-                  <span>{errors.preparation[index].preparation.message}</span>
-                )}
-                <button type="button" onClick={() => remove(index)} className="bg-red-500 text-white rounded p-1">
-                  Remover
-                </button>
+                {errors.name && <span>{errors.name.message}</span>}
               </div>
-            ))}
-            {errors.preparation && <span>{errors.preparation.message}</span>}
-          </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="description">
+                  Description
+                </label>
+                <input
+                  {...register('description')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  type="text"
+                />
+                {errors.description && <span>{errors.description.message}</span>}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="preparation_time">
+                  Preparation time
+                </label>
+                <input
+                  {...register('preparation_time')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  type="text"
+                />
+                {errors.preparation_time && <span>{errors.preparation_time.message}</span>}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="cooking_time">
+                  Cooking time
+                </label>
+                <input
+                  {...register('cooking_time')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  type="text"
+                />
+                {errors.cooking_time && <span>{errors.cooking_time.message}</span>}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="servings">
+                  Servings
+                </label>
+                <input
+                  {...register('servings')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  type="text"
+                />
+                {errors.servings && <span>{errors.servings.message}</span>}
+              </div>
+              <div className="flex flex-col gap-4">
+                <label
+                  className="flex-row flex items-center gap-1 mb-2 text-sm font-medium text-gray-900"
+                  htmlFor="preparation"
+                >
+                  Preparation
+                  <button
+                    type="button"
+                    onClick={addNewTech}
+                    className="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-4 border-b-4 border-emerald-700 hover:border-emerald-500 rounded"
+                  >
+                    adicionar
+                  </button>
+                </label>
+                {fields.map((field, index) => (
+                  <div key={field.id} className="flex items-center gap-2">
+                    <span>{index + 1}</span>
+                    <input
+                      {...register(`preparation.${index}.preparation`)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                      type="text"
+                    />
+                    {errors.preparation?.[index]?.preparation && (
+                      <span>{errors.preparation[index].preparation.message}</span>
+                    )}
+                    <button
+                      type="button"
+                      onClick={() => remove(index)}
+                      className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+                    >
+                      Remover
+                    </button>
+                  </div>
+                ))}
+                {errors.preparation && <span>{errors.preparation.message}</span>}
+              </div>
 
-          <button className="bg-emerald-500 rounded font-semibold p-1" type="submit">
-            Salvar
-          </button>
-        </form>
-        <pre>{output}</pre>
+              <button
+                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                type="submit"
+              >
+                Salvar
+              </button>
+            </form>
+          </div>
+          <div className="border-2 p-4 col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6">
+            <h1>Preview</h1>
+            <pre>{output}</pre>
+          </div>
+        </div>
       </div>
     </>
   );
